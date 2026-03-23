64cac05361a3c6e57953a87a2d8811acb80baa56
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-23 20:15:23 -0300

    feat(docs): setup swagger on Games service

---
+++ services/games/src/_config/swagger/index.ts
@@ -0,0 +1,26 @@
+import { resolve } from "node:path";
+import { writeFile } from "node:fs/promises";
+import { Logger, type INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export const Swagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle("Service de Games")
+    .setDescription("Documentação referente ao serviço de Games")
+    .setVersion("0.0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("games", app, document);
+
+  if (process.env.NODE_ENV === "development") {
+    const specFile = resolve(__dirname, "../../../swagger.json");
+
+    const spec = JSON.stringify(document, null, 2);
+
+    writeFile(specFile, spec).then(() => {
+      Logger.log("[Games] - Swagger spec generated");
+    });
+  }
+};
