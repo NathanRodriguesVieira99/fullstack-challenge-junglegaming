168aebbaf62029a422877d6492f04fb96d0b38a9
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-23 20:15:00 -0300

    feat(env): add environment configuration with zod validation on games service

---
+++ services/games/src/_config/env/index.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+
+import z from "zod";
+
+export const envSchema = z.object({
+  PORT: z.coerce.number(),
+  DATABASE_URL: z.url().startsWith("postgresql://"),
+  NODE_ENV: z
+    .enum(["development", "test", "production"])
+    .default("development"),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  if (process.env.NODE_ENV === "development") {
+    console.error(
+      "❌ Erro ao validar variáveis de ambiente!",
+      z.treeifyError(_env.error),
+    );
+  }
+  throw new Error("❌ Variáveis de ambiente inválidas!");
+}
+
+export const env = _env.data;
