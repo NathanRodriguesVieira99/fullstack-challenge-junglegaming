965c92078b805ec68112925e7e1e4cf0cc6ea5c3
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-23 20:18:08 -0300

    feat(docs): setup Swagger for Wallets service

---
+++ services/wallets/src/_config/swagger/index.ts
@@ -0,0 +1,26 @@
+import { resolve } from "node:path";
+import { writeFile } from "node:fs/promises";
+import { Logger, type INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export const Swagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle("Service de Wallets")
+    .setDescription("Documentação referente ao serviço de Wallet")
+    .setVersion("0.0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("wallets", app, document);
+
+  if (process.env.NODE_ENV === "development") {
+    const specFile = resolve(__dirname, "../../../swagger.json");
+
+    const spec = JSON.stringify(document, null, 2);
+
+    writeFile(specFile, spec).then(() => {
+      Logger.log("[Wallets] - Swagger spec generated");
+    });
+  }
+};
