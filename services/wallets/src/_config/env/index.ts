037b345268e685a7760543f74b1dadd255c49536
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-23 20:17:43 -0300

    feat(env): add environment configuration and validation for Wallets service

---
+++ services/wallets/src/_config/env/index.ts
@@ -0,0 +1,26 @@
+import { loadEnvFile } from "node:process";
+import z from "zod";
+
+loadEnvFile(".env");
+
+export const envSchema = z.object({
+  PORT: z.coerce.number(),
+  DATABASE_URL: z.url().startsWith("postgresql://"),
+  NODE_ENV: z
+    .enum(["development", "test", "production"])
+    .default("development"),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  if (process.env.NODE_ENV === "development") {
+    console.error(
+      "❌ Erro ao validar variáveis de ambiente!",
+      z.treeifyError(_env.error),
+    );
+  }
+  throw new Error("❌ Variáveis de ambiente inválidas!");
+}
+
+export const env = _env.data;
--- services/wallets/src/main.ts
+++ services/wallets/src/main.ts
@@ -1,10 +1,11 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { env } from "./_config/env";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT;
+  const port = env.PORT;
   await app.listen(port, "0.0.0.0");
   console.log(`Wallets service running on port ${port}`);
 }
