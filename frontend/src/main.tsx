ae958cd795783a4cd5e89a0ed1366a9dfb6c8f2a
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-23 21:23:29 -0300

    feat: setup tan stack query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@styles/index.css";
 import App from "./App.tsx";
+import { TanStackQueryProvider } from "./integrations/tanstack-query.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TanStackQueryProvider>
+      <App />
+    </TanStackQueryProvider>
   </StrictMode>,
 );
