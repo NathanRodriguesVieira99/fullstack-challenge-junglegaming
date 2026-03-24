3210bc9d54479f22f2b2ffd157ab1c5e4bf58b32
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-23 21:03:14 -0300

    feat(vite): setup tailwind css and ts paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import babel from "@rolldown/plugin-babel";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tsConfigPaths(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
