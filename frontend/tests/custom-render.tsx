0537aa87368359fd6253fab6092db3267db73d74
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-24 09:46:42 -0300

    feat(tests): add custom render and custom Wrapper with TanStack Query provider

---
+++ frontend/tests/custom-render.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react-refresh/only-export-components */
+import type { FC, ReactElement, ReactNode } from "react";
+import { render, RenderOptions } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+type UI = ReactElement | string;
+
+interface Options extends Omit<RenderOptions, "wrapper"> {
+  props?: Record<string, unknown>;
+}
+
+type Wrapper = { children: ReactNode };
+
+export const Wrapper: FC<Wrapper> = ({ children }) => {
+  const client = new QueryClient();
+
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+};
+
+export const CustomRender = (ui: UI, options: Options) => {
+  return render(ui as ReactElement, options);
+};
+
+export * from "@testing-library/react";
+export * from "@testing-library/user-event";
+export { CustomRender as render };
