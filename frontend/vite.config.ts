b08ab05c94f36068053aa4be6e338190d1c39369
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-23 21:23:12 -0300

    fix(vite): remove tsConfigPaths plugin to use native solution

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import babel from "@rolldown/plugin-babel";
 import path from "node:path";
@@ -8,12 +7,12 @@ import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tsConfigPaths(),
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
   resolve: {
+    tsconfigPaths: true,
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
