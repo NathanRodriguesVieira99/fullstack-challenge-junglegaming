358591cde607e829227454f3b018045c8ba86311
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-24 09:45:41 -0300

    feat: add vitest configuration

---
+++ frontend/vitest.config.ts
@@ -0,0 +1,32 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["**/*.{spec,test}.{ts,tsx}"],
+    reporters: ["verbose"],
+    environment: "jsdom",
+    root: "./",
+    globals: true,
+    restoreMocks: true,
+    clearMocks: true,
+    testTimeout: 10000,
+    setupFiles: [resolve(__dirname, "vitest.setup.ts")],
+
+    coverage: {
+      reportsDirectory: "./coverage",
+      reporter: ["text", "html", "lcov", "cobertura"],
+      provider: "v8",
+      include: ["src/**/*.{ts,tsx}"],
+
+      thresholds: {
+        global: {
+          branches: 70,
+          functions: 70,
+          lines: 70,
+          statements: 70,
+        },
+      },
+    },
+  },
+});
