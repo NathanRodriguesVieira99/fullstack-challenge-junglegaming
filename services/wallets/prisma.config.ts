bc9e3492486bd3a7f0885b7abeeb4be855aa4b6e
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-24 13:23:12 -0300

    feat(prisma): update prisma config file to accept multiple schema files

--- services/wallets/prisma.config.ts
+++ services/wallets/prisma.config.ts
@@ -2,8 +2,12 @@ import { env } from "./src/_config/env";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "prisma",
   migrations: {
+    /*
+     Pasta migrations gerada dentro do Docker pois rodei o script `bun prisma migrate dev` 
+     via `docker compose wallets sh` pois o postgresql está configurado para rodar via Docker.
+    */
     path: "prisma/migrations",
     seed: "tsx ./prisma/seed.ts",
   },
