b88323cc2f68182d3aff2a514d435e79f28d24a0
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-24 11:14:21 -0300

    feat: update AppModule to include Controllers and Services

--- services/games/src/app.module.ts
+++ services/games/src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from "@nestjs/common";
-import { GamesController } from "./presentation/controllers/games.controller";
+
+import { GamesController } from "@controllers/games/games.controller";
+import { HealthController } from "@controllers/health/health.controller";
+
+import { BetService } from "@services/games/bet/bet.service";
+import { BetsService } from "@services/games/bets/bets.service";
+import { RoundsService } from "@services/games/rounds/rounds.service";
 
 @Module({
-  controllers: [GamesController],
+  controllers: [GamesController, HealthController],
+  providers: [BetService, BetsService, RoundsService],
 })
 export class AppModule {}
