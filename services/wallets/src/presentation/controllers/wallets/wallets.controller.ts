754c0d173d644d6c58cabc0d30f584b1018d51f3
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-24 11:25:40 -0300

    style: format WalletsController file

--- services/wallets/src/presentation/controllers/wallets/wallets.controller.ts
+++ services/wallets/src/presentation/controllers/wallets/wallets.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post } from "@nestjs/common";
 
 @Controller("wallets")
 export class WalletsController {
-  @Post("")
+  @Post()
   async createPlayerWallet() {}
 
   @Get("me")
