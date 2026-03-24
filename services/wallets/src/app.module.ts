3c251e96ac86fd2aa539c40b6d953efab0c4adef
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-24 11:22:48 -0300

    feat: update AppModule to include Controllers and Services

--- services/wallets/src/app.module.ts
+++ services/wallets/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from "@nestjs/common";
-import { WalletsController } from "./presentation/controllers/wallets.controller";
+
+import { HealthController } from "@controllers/health/health.controller";
+import { WalletsController } from "@controllers/wallets/wallets.controller";
+
+import { CreateWalletsService } from "@services/wallets/create-wallets.service";
+import { GetWalletsService } from "@services/wallets/get-wallet.service";
 
 @Module({
-  controllers: [WalletsController],
+  controllers: [HealthController, WalletsController],
+  providers: [CreateWalletsService, GetWalletsService],
 })
 export class AppModule {}
