e17607738fc9d63f16b94b323c228281872cb2e8
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-24 19:22:09 -0300

    feat(microservices): configure Kafka producer and consumer in wallets service

--- services/wallets/src/app.module.ts
+++ services/wallets/src/app.module.ts
@@ -5,8 +5,23 @@ import { WalletsController } from "@controllers/wallets/wallets.controller";
 
 import { CreateWalletsService } from "@services/wallets/create-wallets.service";
 import { GetWalletsService } from "@services/wallets/get-wallet.service";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 
 @Module({
+  imports: [
+    /* usado para transformar o service em producer */
+    ClientsModule.register([
+      {
+        name: "wallets-producer",
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ["kafka:29092"], // fora do Docker => localhost:9092
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [HealthController, WalletsController],
   providers: [CreateWalletsService, GetWalletsService],
 })
