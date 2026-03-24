bf14de7cd52a0d8124801a272df5da51a39642a1
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-24 11:19:29 -0300

    feat(wallets): add WalletsController with initial endpoints

---
+++ services/wallets/src/presentation/controllers/wallets/wallets.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, Post } from "@nestjs/common";
+
+@Controller("wallets")
+export class WalletsController {
+  @Post("")
+  async createPlayerWallet() {}
+
+  @Get("me")
+  async getPlayerBalance() {}
+}
