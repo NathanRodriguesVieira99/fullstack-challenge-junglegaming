36d1e93141ed7683eb86a39b72b0b42e3abe6711
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-25 00:02:37 -0300

    docs(health): document health-check response dto with swagger on Games and Wallets service

--- services/wallets/src/presentation/dtos/health-check-response.dto.ts
+++ services/wallets/src/presentation/dtos/health-check-response.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class HealthCheckResponseDto {
+  @ApiProperty({ example: 200 })
   status: string;
+
+  @ApiProperty({ example: "ok" })
   service: string;
 }
