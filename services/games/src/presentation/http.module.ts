8730df0298a381060b1d34b11e88df6363e11a01
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-25 21:22:42 -0300

    chore(auth): remove unused AuthController and AuthService from httpModule

--- services/games/src/presentation/http.module.ts
+++ services/games/src/presentation/http.module.ts
@@ -3,10 +3,8 @@ import { Module } from "@nestjs/common";
 import { BetService } from "@services/games/bet/bet.service";
 import { BetsService } from "@services/games/bets/bets.service";
 import { RoundsService } from "@services/games/rounds/rounds.service";
-import { AuthService } from "../infrastructure/auth/auth.service";
 
 import { HealthController } from "@controllers/health/health.controller";
-import { AuthController } from "../infrastructure/auth/auth.controller";
 import { betController } from "@controllers/games/bet/bet.controller";
 import { betsController } from "@controllers/games/bets/bets.controller";
 import { roundsController } from "@controllers/games/rounds/rounds.controller";
@@ -14,11 +12,10 @@ import { roundsController } from "@controllers/games/rounds/rounds.controller";
 @Module({
   controllers: [
     HealthController,
-    AuthController,
     betController,
     betsController,
     roundsController,
   ],
-  providers: [BetService, BetsService, RoundsService, AuthService],
+  providers: [BetService, BetsService, RoundsService],
 })
 export class httpModule {}
