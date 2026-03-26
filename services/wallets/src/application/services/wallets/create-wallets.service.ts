e598c7752fd0d4ee2226037dd4e60aff66e8ec3f
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-26 10:14:26 -0300

    feat: inject repo and db dependencies on wallet relation services

--- services/wallets/src/application/services/wallets/create-wallets.service.ts
+++ services/wallets/src/application/services/wallets/create-wallets.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import type { WalletsRepositoryImplementation } from "@repos/wallets/wallets.repository.implementation";
+import type { DatabaseService } from "@db/database.service";
 
 @Injectable()
-export class CreateWalletsService {}
+export class CreateWalletService {
+  constructor(
+    private readonly repo: WalletsRepositoryImplementation,
+    private readonly db: DatabaseService,
+  ) {}
+
+  async execute() {}
+}
--- services/wallets/src/application/services/wallets/get-wallet.service.ts
+++ services/wallets/src/application/services/wallets/get-wallet.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import type { WalletsRepositoryImplementation } from "@repos/wallets/wallets.repository.implementation";
+import type { DatabaseService } from "@db/database.service";
 
 @Injectable()
-export class GetWalletsService {}
+export class GetWalletService {
+  constructor(
+    private readonly repo: WalletsRepositoryImplementation,
+    private readonly db: DatabaseService,
+  ) {}
+
+  async execute() {}
+}
