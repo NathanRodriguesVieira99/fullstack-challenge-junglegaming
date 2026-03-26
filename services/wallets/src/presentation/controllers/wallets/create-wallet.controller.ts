55dcfc224cb9d0420cc9388a261f82686b370fed
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-26 15:19:34 -0300

    feat(wallets): implement service on CreateWalletController with JWT authentication and add Swagger docs

--- services/wallets/src/presentation/controllers/wallets/create-wallet.controller.ts
+++ services/wallets/src/presentation/controllers/wallets/create-wallet.controller.ts
@@ -1,9 +1,51 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { CreateWalletService } from "@services/wallets/create-wallets.service";
+import {
+  Req,
+  Controller,
+  UseGuards,
+  Post,
+  HttpCode,
+  HttpStatus,
+} from "@nestjs/common";
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from "@nestjs/swagger";
+import { JwtGuard } from "@/infrastructure/auth/jwt/jwt.guard";
+import { CreateWalletService } from "@/application/services/wallets/create-wallet.service";
+import { createWalletDto } from "@/presentation/dtos/wallet.dto";
 
 @ApiTags("Wallets")
+@UseGuards(JwtGuard)
 @Controller("wallets")
 export class CreateWalletController {
   constructor(private readonly service: CreateWalletService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @ApiBearerAuth()
+  @ApiOperation({
+    operationId: "CreateWallet",
+    description: "Cria uma carteira para o jogador",
+    summary:
+      "Cria uma carteira para usuário autenticado com 1000 de saldo inicial",
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: "Carteira criada com sucesso",
+    type: createWalletDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: "Usuário não autorizado (Token JWT inválido ou ausente)",
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: "Usuário já possui uma carteira",
+  })
+  create(@Req() req: { user: { userId: string } }) {
+    const playerId = req.user.userId;
+    return this.service.execute({ playerId, balance: 1000n });
+  }
 }
