6794bf69b3b1df398d6d8cf39196ea42499ed078
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-26 15:16:56 -0300

    refactor: refactor http module to include Kafka producer configuration and update service imports

--- services/wallets/src/app.module.ts
+++ services/wallets/src/app.module.ts
@@ -1,30 +1,9 @@
-import { ClientsModule, Transport } from "@nestjs/microservices";
-
 import { Module } from "@nestjs/common";
-
 import { DatabaseModule } from "@db/database.module";
-
 import { AuthModule } from "./infrastructure/auth/auth.module";
-
 import { httpModule } from "./presentation/http.module";
 
 @Module({
-  imports: [
-    httpModule,
-    AuthModule,
-    DatabaseModule,
-    /* usado para transformar o service em producer */
-    ClientsModule.register([
-      {
-        name: "wallets-producer",
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            brokers: ["kafka:29092"], // fora do Docker => localhost:9092
-          },
-        },
-      },
-    ]),
-  ],
+  imports: [httpModule, AuthModule, DatabaseModule],
 })
 export class AppModule {}
--- services/wallets/src/presentation/http.module.ts
+++ services/wallets/src/presentation/http.module.ts
@@ -1,14 +1,36 @@
+import { ClientsModule, Transport } from "@nestjs/microservices";
 import { Module } from "@nestjs/common";
 
 import { HealthController } from "./controllers/health/health.controller";
 import { GetWalletController } from "./controllers/wallets/get-wallet.controller";
 import { CreateWalletController } from "./controllers/wallets/create-wallet.controller";
 
-import { CreateWalletService } from "@/application/services/wallets/create-wallets.service";
+import { CreateWalletService } from "@/application/services/wallets/create-wallet.service";
 import { GetWalletService } from "@/application/services/wallets/get-wallet.service";
 
+import { WalletsRepositoryImplementation } from "@repos/wallets/wallets.repository.implementation";
+
 @Module({
+  imports: [
+    /* usado para transformar o service em producer */
+    ClientsModule.register([
+      {
+        name: "wallets-producer",
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: "WALLETS_SERVICE_PRODUCER",
+            brokers: ["kafka:29092"], // fora do Docker => localhost:9092
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [HealthController, GetWalletController, CreateWalletController],
-  providers: [CreateWalletService, GetWalletService],
+  providers: [
+    WalletsRepositoryImplementation,
+    CreateWalletService,
+    GetWalletService,
+  ],
 })
 export class httpModule {}
