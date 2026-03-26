0b8ee3614b347396dab136d50083e10ac05e15fd
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-26 17:27:06 -0300

    test(wallet): create unit tests with happy-path, error handling and Kafka event emission

--- services/wallets/tests/unit/application/services/wallets/create-wallet.service.spec.ts
+++ services/wallets/tests/unit/application/services/wallets/create-wallet.service.spec.ts
@@ -1,19 +1,140 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { describe, beforeEach, it, expect, vi } from "vitest";
-import { CreateWalletService } from "../../../../../src/application/services/wallets/create-wallets.service";
+import { CreateWalletService } from "../../../../../src/application/services/wallets/create-wallet.service";
+import { WalletsRepositoryContract } from "../../../../../src/domain/repositories/wallets/wallets.repository.contract";
+import { createWalletDto } from "@/presentation/dtos/wallet.dto";
+import {
+  ConflictException,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
+
+const mockWalletRepository = {
+  createWallet: vi.fn(),
+};
+
+const mockKafkaProducer = {
+  emit: vi.fn(),
+};
+
+const wallet: createWalletDto & { id: string } = {
+  id: "uuid-super-aleatório",
+  playerId: "player123",
+  balance: 1000n,
+};
 
 describe("CreateWalletService", () => {
   let service: CreateWalletService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateWalletService],
+      providers: [
+        CreateWalletService,
+        {
+          provide: WalletsRepositoryContract,
+          useValue: mockWalletRepository,
+        },
+        {
+          provide: "wallets-producer",
+          useValue: mockKafkaProducer,
+        },
+      ],
     }).compile();
 
     service = module.get<CreateWalletService>(CreateWalletService);
   });
 
-  it("should be defined", () => {
-    expect(service).toBeDefined();
+  describe("Create Wallet", () => {
+    it("should be defined", () => {
+      expect(service).toBeDefined();
+    });
+
+    describe("Kafka", () => {
+      it('should emit "wallet.created" kafka event', async () => {
+        mockWalletRepository.createWallet.mockResolvedValue(wallet);
+        mockKafkaProducer.emit.mockResolvedValue(undefined);
+
+        await service.execute(wallet);
+
+        expect(mockKafkaProducer.emit).toHaveBeenCalledWith("wallet.created", {
+          playerId: wallet.playerId,
+          walletId: wallet.id,
+        });
+      });
+    });
+
+    describe("Happy-Paths", () => {
+      it("should create a wallet with initial balance of 1000", async () => {
+        mockWalletRepository.createWallet.mockResolvedValue(wallet);
+
+        const result = await service.execute(wallet);
+
+        console.log(result);
+        expect(result).toHaveProperty("balance");
+        expect(result).toHaveProperty("playerId");
+        expect(result.balance).toBe(1000n);
+      });
+    });
+
+    describe("Errors", () => {
+      it("should throw Conflict Exception when create duplicated wallet", async () => {
+        // cria pela primeira vez
+        mockWalletRepository.createWallet.mockResolvedValue(wallet);
+
+        // cria pela segunda vez
+        mockWalletRepository.createWallet.mockRejectedValue(
+          new ConflictException("Wallet already exists"),
+        );
+
+        await expect(service.execute(wallet)).rejects.toThrow(
+          new ConflictException("Wallet already exists"),
+        );
+      });
+
+      it("should throw Unauthorized Exception when balance is < 1 ", async () => {
+        const balanceNegative = {
+          balance: 0n,
+          playerId: "player123",
+        };
+
+        mockWalletRepository.createWallet.mockRejectedValueOnce(
+          new UnauthorizedException("Balance should not be negative"),
+        );
+
+        await expect(service.execute(balanceNegative)).rejects.toThrow(
+          new UnauthorizedException("Balance should not be negative"),
+        );
+      });
+
+      it("should throw Unauthorized Exception when balance is > 1000 ", async () => {
+        const balanceOverOneThousand = {
+          balance: 1001n,
+          playerId: "player123",
+        };
+
+        mockWalletRepository.createWallet.mockRejectedValueOnce(
+          new UnauthorizedException("Balance should not be negative"),
+        );
+
+        await expect(service.execute(balanceOverOneThousand)).rejects.toThrow(
+          new UnauthorizedException("Balance should not be negative"),
+        );
+      });
+
+      it("should throw Not Found Exception when user not exists", async () => {
+        const invalidData = {
+          balance: 100n,
+          playerId: "",
+        };
+
+        mockWalletRepository.createWallet.mockRejectedValueOnce(
+          new NotFoundException("User not found"),
+        );
+
+        await expect(service.execute(invalidData)).rejects.toThrow(
+          new NotFoundException("User not found"),
+        );
+      });
+    });
   });
 });
