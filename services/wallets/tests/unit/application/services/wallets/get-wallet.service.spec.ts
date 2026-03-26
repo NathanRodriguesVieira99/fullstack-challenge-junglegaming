9129c76c8c9e081814661dd40deebea4e7b603c8
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-26 17:44:39 -0300

    test: enhance GetWalletService tests with mock repository and kafka producer

--- services/wallets/tests/unit/application/services/wallets/get-wallet.service.spec.ts
+++ services/wallets/tests/unit/application/services/wallets/get-wallet.service.spec.ts
@@ -1,13 +1,32 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { describe, beforeEach, it, expect, vi } from "vitest";
 import { GetWalletService } from "../../../../../src/application/services/wallets/get-wallet.service";
+import { WalletsRepositoryContract } from "../../../../../src/domain/repositories/wallets/wallets.repository.contract";
+
+const mockWalletRepositoryImplementation = {
+  getWalletByPlayerId: vi.fn(),
+};
+
+const mockKafkaProducer = {
+  emit: vi.fn(),
+};
 
 describe("GetWalletService", () => {
   let service: GetWalletService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GetWalletService],
+      providers: [
+        GetWalletService,
+        {
+          provide: WalletsRepositoryContract,
+          useValue: mockWalletRepositoryImplementation,
+        },
+        {
+          provide: "wallets-producer",
+          useValue: mockKafkaProducer,
+        },
+      ],
     }).compile();
 
     service = module.get<GetWalletService>(GetWalletService);
