b1978bf1a69830fc472d25424d6438ee8bfbc076
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-27 17:34:49 -0300

    feat: add Credit and Debit controllers and services to httpModule

--- services/wallets/src/presentation/http.module.ts
+++ services/wallets/src/presentation/http.module.ts
@@ -1,15 +1,24 @@
 import { ClientsModule, Transport } from "@nestjs/microservices";
 import { Module } from "@nestjs/common";
 
+/*  Controllers */
 import { HealthController } from "../presentation/controllers/health/health.controller";
 import { GetWalletController } from "../presentation/controllers/wallets/get-wallet.controller";
 import { CreateWalletController } from "../presentation/controllers/wallets/create-wallet.controller";
+import { CreditController } from "./controllers/transactions/credit.controller";
+import { DebitController } from "./controllers/transactions/debit.controller";
 
+/*  Services */
 import { CreateWalletService } from "../application/services/wallets/create-wallet.service";
 import { GetWalletService } from "../application/services/wallets/get-wallet.service";
+import { CreditService } from "../application/services/transactions/credit.service";
+import { DebitService } from "../application/services/transactions/debit.service";
 
+/*  Repositories */
 import { WalletsRepositoryContract } from "../domain/repositories/wallets/wallets.repository.contract";
 import { WalletsRepositoryImplementation } from "../domain/repositories/wallets/wallets.repository.implementation";
+import { TransactionRepositoryContract } from "../domain/repositories/transactions/transactions.repository.contract";
+import { TransactionsRepositoryImplementation } from "../domain/repositories/transactions/transactions.repository.implementation";
 
 @Module({
   imports: [
@@ -27,14 +36,26 @@ import { WalletsRepositoryImplementation } from "../domain/repositories/wallets/
       },
     ]),
   ],
-  controllers: [HealthController, GetWalletController, CreateWalletController],
+  controllers: [
+    HealthController,
+    GetWalletController,
+    CreateWalletController,
+    CreditController,
+    DebitController,
+  ],
   providers: [
     {
       provide: WalletsRepositoryContract,
       useClass: WalletsRepositoryImplementation,
     },
+    {
+      provide: TransactionRepositoryContract,
+      useClass: TransactionsRepositoryImplementation,
+    },
     CreateWalletService,
     GetWalletService,
+    DebitService,
+    CreditService,
   ],
 })
 export class httpModule {}
