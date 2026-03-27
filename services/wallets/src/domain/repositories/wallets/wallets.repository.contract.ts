4673f968cf73c707d48fefefb775f953a6622110
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-27 17:36:17 -0300

    refactor: update createWallet method signature to use CreateWalletRequestDto and return CreateWalletResponseDto

--- services/wallets/src/domain/repositories/wallets/wallets.repository.contract.ts
+++ services/wallets/src/domain/repositories/wallets/wallets.repository.contract.ts
@@ -1,38 +1,21 @@
-import type { Player } from "@/domain/entities/player";
-import { Transaction } from "@/domain/entities/transaction";
 import { Wallet } from "@/domain/entities/wallet";
-import { createWalletDto } from "@dtos/wallet.dto";
+import {
+  CreateWalletRequestDto,
+  CreateWalletResponseDto,
+} from "@dtos/wallet.dto";
 
 export interface IWalletsRepository {
-  createWallet({ playerId, balance }: createWalletDto): Promise<Wallet>;
+  createWallet({
+    playerId,
+    balance,
+  }: CreateWalletRequestDto): Promise<CreateWalletResponseDto>;
   getWalletByPlayerId(playerId: string): Promise<Wallet | null>;
-  // getByPlayerId(playerId: string): Promise<Wallet | null>;
-  // creditTransaction(
-  //   transactionId: string,
-  //   walletId: string,
-  //   amount: bigint,
-  // ): Promise<Transaction>;
-  // debitTransaction(
-  //   transactionId: string,
-  //   walletId: string,
-  //   amount: bigint,
-  // ): Promise<Transaction>;
 }
 
 export abstract class WalletsRepositoryContract implements IWalletsRepository {
   abstract createWallet({
     playerId,
     balance,
-  }: createWalletDto): Promise<Wallet>;
+  }: CreateWalletRequestDto): Promise<CreateWalletResponseDto>;
   abstract getWalletByPlayerId(playerId: string): Promise<Wallet | null>;
-  // abstract creditTransaction(
-  //   transactionId: string,
-  //   walletId: string,
-  //   amount: bigint,
-  // ): Promise<Transaction>;
-  // abstract debitTransaction(
-  //   transactionId: string,
-  //   walletId: string,
-  //   amount: bigint,
-  // ): Promise<Transaction>;
 }
