a828161a7e4c5ec9457f57336b365485fad4d019
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-27 17:37:30 -0300

    refactor: update execute method to use CreateWalletRequestDto for improved type safety

--- services/wallets/src/application/services/wallets/create-wallet.service.ts
+++ services/wallets/src/application/services/wallets/create-wallet.service.ts
@@ -1,6 +1,7 @@
-import { WalletsRepositoryContract } from "../../../domain/repositories/wallets/wallets.repository.contract";
-import type { createWalletDto } from "../../../presentation/dtos/wallet.dto";
 import { Inject, Injectable } from "@nestjs/common";
+import { WalletsRepositoryContract } from "../../../domain/repositories/wallets/wallets.repository.contract";
+
+import type { CreateWalletRequestDto } from "../../../presentation/dtos/wallet.dto";
 import type { ClientKafka } from "@nestjs/microservices";
 
 @Injectable()
@@ -10,7 +11,7 @@ export class CreateWalletService {
     @Inject("wallets-producer") private readonly kafka: ClientKafka,
   ) {}
 
-  async execute({ playerId, balance }: createWalletDto) {
+  async execute({ playerId, balance }: CreateWalletRequestDto) {
     const wallet = await this.repo.createWallet({ playerId, balance });
 
     this.kafka.emit("wallet.created", { playerId, walletId: wallet.id });
