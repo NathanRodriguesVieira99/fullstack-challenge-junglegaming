58878ef389124d5885d918405c12654f96a08b3f
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-27 14:49:56 -0300

    chore: remove unused sanitizeBigInt utility function

--- services/wallets/src/utils/sanitize-bigInt.ts
+++
@@ -1,8 +0,0 @@
-// A IA me ajudou a achar essa solução pois o NestJs aparentemente nao consegue lidar muito bem com  BigInt
-export const sanitizeBigInt = (obj: any): any => {
-  return JSON.parse(
-    JSON.stringify(obj, (key, value) =>
-      typeof value === "bigint" ? value.toString() : value,
-    ),
-  );
-};
