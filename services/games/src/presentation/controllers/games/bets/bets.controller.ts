81d481b9adfa369bb2dde4554e727a53db94aaa5
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-28 16:59:54 -0300

    refactor(bets): remove BetsService and betsController as they are no longer needed

--- services/games/src/presentation/controllers/games/bets/bets.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-
-@Controller("games")
-@ApiTags("Bets")
-export class betsController {
-  @Get("bets/me")
-  async bets() {}
-}
