3c8d2b64e3c24588ca44364f68604e60ff6eaa29
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-28 13:11:54 -0300

    fix(orval): correct target path and mode in orval configuration

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -7,8 +7,9 @@ export default defineConfig({
       baseUrl: process.env.BASE_URL || "http://localhost:4002",
       client: "react-query",
       httpClient: "axios",
-      target: "src/api/http/generated/wallets.ts",
-      mode: "tags-split",
+      target: "src/api/http/generated/wallets/wallets.ts",
+      mode: "split",
+
       namingConvention: "kebab-case",
       indexFiles: true,
       clean: true,
