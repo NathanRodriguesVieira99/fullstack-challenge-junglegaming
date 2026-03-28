d7afac4a5b043bd155daaa0a53703a1a865ac9a8
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-28 18:14:29 -0300

    refactor(debit): update message pattern to use KAFKA_TOPICS constant

--- services/wallets/src/presentation/controllers/transactions/debit.controller.ts
+++ services/wallets/src/presentation/controllers/transactions/debit.controller.ts
@@ -1,11 +1,12 @@
 import { MessagePattern, Payload } from "@nestjs/microservices";
 import { Body, Controller, Post, UseGuards } from "@nestjs/common";
-import { ApiBody, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiTags } from "@nestjs/swagger";
 
 import { JwtGuard } from "@/infrastructure/auth/jwt/jwt.guard";
 
 import { DebitService } from "../../../application/services/transactions/debit.service";
 import { DebitRequestDto, DebitResponseDto } from "../../dtos/debit.dto";
+import { KAFKA_TOPICS } from "@/constants/kafka";
 
 @ApiTags("debit")
 @UseGuards(JwtGuard)
@@ -13,7 +14,8 @@ import { DebitRequestDto, DebitResponseDto } from "../../dtos/debit.dto";
 export class DebitController {
   constructor(private readonly service: DebitService) {}
 
-  @MessagePattern("debit.transaction")  async debitMessage(
+  @MessagePattern(KAFKA_TOPICS.DEBIT_TRANSACTION)
+  async debitMessage(
     @Payload() payload: DebitRequestDto,
   ): Promise<DebitResponseDto> {
     return this.service.execute(payload);
