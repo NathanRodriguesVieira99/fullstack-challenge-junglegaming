808a674b92b830d562f20edd635ee2d2cf4eb18b
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-28 18:16:25 -0300

    test(bet-service): update tests to use CreateBetService and remove BetsService

--- services/games/tests/unit/application/services/games/bet/bet.service.spec.ts
+++ services/games/tests/unit/application/services/games/bet/bet.service.spec.ts
@@ -1,16 +1,29 @@
-import { describe, beforeEach, it, expect } from "vitest";
+import { describe, beforeEach, it, expect, vi } from "vitest";
 import { Test, TestingModule } from "@nestjs/testing";
-import { BetService } from "../../../../../../src/application/services/games/bet/bet.service";
+import { CreateBetService } from "../../../../../../src/application/services/games/bet/create-bet.service";
+
+import { KAFKA_CLIENTS } from "../../../../../../src/constants/kafka";
+import { BetsRepositoryContract } from "../../../../../../src/domain/repositories/bets/bets.repository.contract";
 
 describe("Bet Service", () => {
-  let service: BetService;
+  let service: CreateBetService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BetService],
+      providers: [
+        CreateBetService,
+        {
+          provide: KAFKA_CLIENTS.PRODUCER,
+          useValue: { emit: vi.fn() },
+        },
+        {
+          provide: BetsRepositoryContract,
+          useValue: { createBet: vi.fn() },
+        },
+      ],
     }).compile();
 
-    service = module.get<BetService>(BetService);
+    service = module.get<CreateBetService>(CreateBetService);
   });
 
   it("should be defined", () => {
