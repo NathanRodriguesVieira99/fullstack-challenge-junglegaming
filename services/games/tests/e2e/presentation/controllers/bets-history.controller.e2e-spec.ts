7d7f2fb133b13b67050ba350608b7f9677a735cd
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-28 18:17:30 -0300

    test(e2e): add initial e2e test file

---
+++ services/games/tests/e2e/presentation/controllers/bets-history.controller.e2e-spec.ts
@@ -0,0 +1,19 @@
+import { describe, beforeEach, it, expect } from "vitest";
+import { Test, TestingModule } from "@nestjs/testing";
+import { betsHistoryController } from "../../../../src/presentation/controllers/games/bets/bets-history.controller";
+
+describe("betsHistoryController", () => {
+  let controller: betsHistoryController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [betsHistoryController],
+    }).compile();
+
+    controller = module.get<betsHistoryController>(betsHistoryController);
+  });
+
+  it("should be defined", () => {
+    expect(controller).toBeDefined();
+  });
+});
