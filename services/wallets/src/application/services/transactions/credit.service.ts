f2fd6e9c86d691cf72367906bad348168af27948
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-27 21:31:29 -0300

    feat(transactions): implement credit transaction method and integrate Kafka messaging

--- services/wallets/src/application/services/transactions/credit.service.ts
+++ services/wallets/src/application/services/transactions/credit.service.ts
@@ -1,9 +1,43 @@
 import { Inject, Injectable } from "@nestjs/common";
+import {
+  CreditRequestDto,
+  CreditResponseDto,
+} from "../../../presentation/dtos/credit.dto";
+
 import type { ClientKafka } from "@nestjs/microservices";
+import { KAFKA_CLIENTS, KAFKA_TOPICS } from "../../../constants/kafka";
+
+import { TransactionRepositoryContract } from "../../../domain/repositories/transactions/transactions.repository.contract";
 
 @Injectable()
 export class CreditService {
-  constructor() // @Inject("wallets-producer") private readonly kafka: ClientKafka,
-  {}
-  async execute() {}
+  constructor(
+    @Inject(KAFKA_CLIENTS.PRODUCER)
+    private readonly kafka: ClientKafka,
+    private readonly repo: TransactionRepositoryContract,
+  ) {}
+  async execute({
+    transactionId,
+    transactionValue,
+    walletId,
+    playerId,
+  }: CreditRequestDto): Promise<CreditResponseDto> {
+    const amount = transactionValue;
+
+    const transaction = await this.repo.creditTransaction({
+      transactionId,
+      transactionValue,
+      walletId,
+      playerId,
+    });
+
+    this.kafka.emit(KAFKA_TOPICS.CREDIT_TRANSACTION, {
+      transaction: transactionId,
+      wallet: walletId,
+      player: playerId,
+      credited_value: amount,
+    });
+
+    return transaction;
+  }
 }
